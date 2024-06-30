73b01676597daa43c51befc1484b6da4372b9885
Kholodaet <kholodaet@icloud.com>
2024-06-30 11:39:53 +0300

    add gallery

--- src/components/PhotosGallery/PhotosGallery.jsx
+++ src/components/PhotosGallery/PhotosGallery.jsx
@@ -1,3 +1,19 @@
-export const PhotosGallery = () => {
-  return <h3>PhotosGallery</h3>;
+import { Grid, PhotosGalleryItem } from 'components';
+
+export const PhotosGallery = ({ photos }) => {
+  return (
+    <Grid>
+      {photos.map(photo => {
+        return (
+          <li key={photo.id}>
+            <PhotosGalleryItem
+              avg_color={photo.avg_color}
+              alt={photo.alt}
+              src={photo.src}
+            />
+          </li>
+        );
+      })}
+    </Grid>
+  );
 };

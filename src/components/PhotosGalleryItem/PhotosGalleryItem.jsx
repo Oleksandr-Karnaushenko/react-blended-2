44184c1aa32222c47ffd37e4d0b4e83ec9544b8b
gorisgor <igor.matvienko1989@gmail.com>
2024-06-30 13:07:35 +0300

    ModalImage

--- src/components/PhotosGalleryItem/PhotosGalleryItem.jsx
+++ src/components/PhotosGalleryItem/PhotosGalleryItem.jsx
@@ -1,14 +1,18 @@
 import styles from './PhotosGalleryItem.module.css';
 import { GridItem } from '..';
 
-export const PhotosGalleryItem = ({ avg_color, alt, src }) => {
+export const PhotosGalleryItem = ({ avg_color, alt, src, openModal }) => {
   return (
     <GridItem>
       <div
         className={styles.thumb}
         style={{ backgroundColor: avg_color, borderColor: avg_color }}
       >
-        <img src={src.large} alt={alt} />
+        <img
+          src={src.large}
+          alt={alt}
+          onClick={() => openModal({ src: src.large, alt })}
+        />
       </div>
     </GridItem>
   );
